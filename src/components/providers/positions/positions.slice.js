b7f4464bb1f76553c7e220824605308feca6801f
LanaVol <svolosh4yk@gmail.com>
2023-11-26 17:57:35 +0200

    added new selectors & update reducers

--- src/components/providers/positions/positions.slice.js
+++ src/components/providers/positions/positions.slice.js
@@ -12,6 +12,58 @@ export const positionsSlice = createSlice({
     addNewPosition: (state, { payload }) => {
       state.data = [...state.data, payload];
     },
+    updatePosition: (state, { payload }) => {
+      state.data = state.data.map((position) => {
+        if (position.id === payload.id) {
+          return payload;
+        }
+        return position;
+      });
+    },
+    setCurrentPositionId: (state, { payload }) => {
+      state.currentPositionId = payload;
+    },
+    updatePermits: (state, { payload }) => {
+      const { id, permit } = payload;
+
+      state.data = state.data.map((position) => {
+        if (position.id === id) {
+          const updatedPermits = position.permits.includes(permit)
+            ? position.permits.filter((p) => p !== permit)
+            : [...position.permits, permit];
+
+          return { ...position, permits: updatedPermits };
+        }
+        return position;
+      });
+    },
+
+    // updatePermits: (state, { payload }) => {
+    //   const { id, permit } = payload;
+
+    //   const updatedPosition = state.data.find((position) => position.id === id);
+
+    //   if (!updatedPosition) return;
+
+    //   if (updatedPosition.permits.includes(permit)) {
+    //     updatedPosition.permits.splice(
+    //       updatedPosition.permits.indexOf(permit),
+    //       1
+    //     );
+    //   } else {
+    //     updatedPosition.permits.push(permit);
+    //   }
+
+    //   state.data = state.data.map((position) => {
+    //     if (position.id === id) {
+    //       return updatedPosition;
+    //     }
+    //     return position;
+    //   });
+    // },
+    setDataList: (state, { payload }) => {
+      state.data = payload;
+    },
   },
 });
 
--- src/components/providers/positions/selectors.js
+++ src/components/providers/positions/selectors.js
@@ -1,1 +1,7 @@
 export const getPositionData = (state) => state.positions.data;
+export const getCurrentPositionId = (state) =>
+  state.positions.currentPositionId;
+export const getActivePositionInfo = (state) =>
+  state.positions.data.find(
+    (position) => position.id === state.positions.currentPositionId
+  );
